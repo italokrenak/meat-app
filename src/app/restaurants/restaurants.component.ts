c5ee4ea8b8d13f1c3b45f9f892306d1eeef7dd2a
italokrenak <italokrenak@gmail.com>
2018-11-02 14:06:41 -0300

    Creating Restaurant Component to list restaurants dynamically

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Restaurant } from './restaurant/restaurant.model';
+
 @Component({
-  selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+    selector: 'mt-restaurants',
+    templateUrl: './restaurants.component.html'
 })
 export class RestaurantsComponent implements OnInit {
 
-  constructor() { }
+    restaurants: Restaurant[] = [
+        {
+            id: 'bread-bakery',
+            name: 'Bread & Bakery',
+            category: 'Bakery',
+            deliveryEstimate: '25m',
+            rating: 4.9,
+            imagePath: 'assets/img/restaurants/breadbakery.png'
+        },
+        {
+            id: 'burger-house',
+            name: 'Burger House',
+            category: 'Hamburgers',
+            deliveryEstimate: '100m',
+            rating: 3.5,
+            imagePath: 'assets/img/restaurants/burgerhouse.png'
+        }
+    ];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
