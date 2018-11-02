d73a8dd973f6be8d08684762d0e6d769e67436ce
italokrenak <italokrenak@gmail.com>
2018-11-02 13:26:34 -0300

    Creating Restaurants Component to list all restaurants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
 import { ROUTES } from './app.routes';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { ROUTES } from './app.routes';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    RestaurantsComponent
   ],
   imports: [
     BrowserModule,
