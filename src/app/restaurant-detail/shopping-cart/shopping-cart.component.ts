1ee9e88a134070e538d4ac5a7c1b9f6dc9da0809
italokrenak <italokrenak@gmail.com>
2018-11-04 11:05:06 -0300

    Applying Events (Add/Remove) Items to ShoppingCart Component.

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -3,22 +3,34 @@ import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from "./shopping-cart.service";
 
 @Component({
-  selector: 'mt-shopping-cart',
-  templateUrl: './shopping-cart.component.html'
+    selector: 'mt-shopping-cart',
+    templateUrl: './shopping-cart.component.html'
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor(private shoppingCartService: ShoppingCartService) { }
+    constructor(private shoppingCartService: ShoppingCartService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  items(): any[] {
-    return this.shoppingCartService.items;
-  }
+    items(): any[] {
+        return this.shoppingCartService.items;
+    }
 
-  total(): number {
-    return this.shoppingCartService.total();
-  }
+    clear() {
+        this.shoppingCartService.clear();
+    }
+
+    removeItem(item: any) {
+        this.shoppingCartService.removeItem(item);
+    }
+
+    addItem(item: any) {
+        this.shoppingCartService.addItem(item);
+    }
+
+    total(): number {
+        return this.shoppingCartService.total();
+    }
 
 }
