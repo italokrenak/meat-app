3e219208430e15b227d8c233869eb0c52435eb30
italokrenak <italokrenak@gmail.com>
2018-11-04 10:52:55 -0300

    Fixing Menu Component to receive an Event from MenuItem Component. (Using EventEmitter)

--- src/app/restaurant-detail/menu/menu.component.ts
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -6,19 +6,23 @@ import { Observable } from 'rxjs/Observable';
 import { MenuItem } from '../menu-item/menu-item.model';
 
 @Component({
-  selector: 'mt-menu',
-  templateUrl: './menu.component.html'
+    selector: 'mt-menu',
+    templateUrl: './menu.component.html'
 })
 export class MenuComponent implements OnInit {
 
-  menu: Observable<MenuItem[]>;
+    menu: Observable<MenuItem[]>;
 
-  constructor(private restaurantsService: RestaurantsService,
-    private route: ActivatedRoute) { }
+    constructor(private restaurantsService: RestaurantsService,
+        private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    this.menu = this.restaurantsService
-      .menuOfRestaurants(this.route.parent.snapshot.params['id']);
-  }
+    ngOnInit() {
+        this.menu = this.restaurantsService
+            .menuOfRestaurants(this.route.parent.snapshot.params['id']);
+    }
+
+    addMenuItem(item: MenuItem) {
+        console.log(item);
+    }
 
 }
