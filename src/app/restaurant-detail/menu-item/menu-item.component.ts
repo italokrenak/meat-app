8b0be7fcbbff88f2fad5f86549d43f6b1e20eca1
italokrenak <italokrenak@gmail.com>
2018-11-04 09:00:45 -0300

    Implementing MenuItems. Using PIPE (Currency and Async)

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+import { MenuItem } from "./menu-item.model";
 
 @Component({
   selector: 'mt-menu-item',
@@ -6,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuItemComponent implements OnInit {
 
+  @Input() menuItem: MenuItem;
+  @Output() add = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  emitAddEvent() {
+    this.add.emit(this.menuItem);
   }
 
 }
