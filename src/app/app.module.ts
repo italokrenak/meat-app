e9a253d6573d381123235d01d28c37dea462abaf
italokrenak <italokrenak@gmail.com>
2018-11-07 21:35:30 -0300

    Creating Input Component using (Content Projection)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
 
 @NgModule({
     declarations: [
@@ -34,7 +35,8 @@ import { OrderComponent } from './order/order.component';
         MenuItemComponent,
         ShoppingCartComponent,
         ReviewsComponent,
-        OrderComponent
+        OrderComponent,
+        InputComponent
     ],
     imports: [
         BrowserModule,
