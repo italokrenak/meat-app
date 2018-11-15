d3b60ef2107f18ac13b5824460ad0489a1217c16
italokrenak <italokrenak@gmail.com>
2018-11-15 13:38:35 -0300

    Implementing Component Purchase Items

---
+++ src/app/order/order-items/order-items.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+
+@Component({
+    selector: 'mt-order-items',
+    templateUrl: './order-items.component.html'
+})
+export class OrderItemsComponent implements OnInit {
+
+    @Input() items: CartItem[];
+
+    @Output() increaseQty = new EventEmitter<CartItem>();
+
+    @Output() decreaseQty = new EventEmitter<CartItem>();
+
+    @Output() removeQty = new EventEmitter<CartItem>();
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    emitIncreaseQty(item: CartItem) {
+        this.increaseQty.emit(item);
+    }
+
+    emitDecreaseQty(item: CartItem) {
+        this.decreaseQty.emit(item);
+    }
+
+    emitRemove(item: CartItem) {
+        this.removeQty.emit(item);
+    }
+
+}
