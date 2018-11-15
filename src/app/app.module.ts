44b5386d26490ae8b5a58644400d100539bbd568
italokrenak <italokrenak@gmail.com>
2018-11-15 16:40:59 -0300

    Adding Navegation Afetr finish Purchase (ROUTER)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 @NgModule({
     declarations: [
@@ -43,7 +44,8 @@ import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.co
         InputComponent,
         RadioComponent,
         OrderItemsComponent,
-        DeliveryCostsComponent
+        DeliveryCostsComponent,
+        OrderSummaryComponent
     ],
     imports: [
         BrowserModule,
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,6 +4,7 @@ import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'mt-order',
@@ -19,7 +20,7 @@ export class OrderComponent implements OnInit {
         { label: 'Cartão de Crédito', value: 'REF' }
     ]
 
-    constructor(private orderService: OrderService) { }
+    constructor(private orderService: OrderService, private router: Router) { }
 
     ngOnInit() {
     }
@@ -53,6 +54,7 @@ export class OrderComponent implements OnInit {
             );
 
         this.orderService.checkOrder(order).subscribe((orderId: string) => {
+            this.router.navigate(['/order-summary']);
             console.log(`Compra concluída ${orderId}`);
             this.orderService.clear();
         });
