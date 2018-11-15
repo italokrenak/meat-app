9ef2f5b2e3d8b40754acc6a2dce052d50f6b16d3
italokrenak <italokrenak@gmail.com>
2018-11-15 16:07:24 -0300

    Using [HTTP POST] to finish the purchase

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { Http, RequestOptions, Headers } from "@angular/http";
 
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
+import { Order } from "./order.model";
+import { MEAT_API } from "app/app.api";
 
 @Injectable()
 export class OrderService {
 
-    constructor(private cartService: ShoppingCartService) { }
+    constructor(private cartService: ShoppingCartService, private http: Http) { }
 
     itemsValue(): number {
         return this.cartService.total();
@@ -26,6 +30,18 @@ export class OrderService {
 
     remove(item: CartItem) {
         this.cartService.removeItem(item);
+    }
+
+    clear() {
+        this.cartService.clear();
+    }
+
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({ headers: headers }))
+            .map(response => response.json());
     }
 
 }
