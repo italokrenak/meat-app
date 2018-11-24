d32204ede8562a0ff4eb6e3ae3a3a0ac918e0765
italokrenak <italokrenak@gmail.com>
2018-11-24 11:26:50 -0300

    Creating AboutModule and using LazyLoading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
 import { ROUTES } from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
@@ -33,7 +32,6 @@ import { RattingComponent } from './shared/ratting/ratting.component';
         AppComponent,
         HeaderComponent,
         HomeComponent,
-        AboutComponent,
         RestaurantsComponent,
         RestaurantComponent,
         RestaurantDetailComponent,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,5 +24,5 @@ export const ROUTES: Routes = [
     },
     { path: 'order', component: OrderComponent },
     { path: 'order-summary', component: OrderSummaryComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', loadChildren: './about/about.module#AboutModule' }
 ]
