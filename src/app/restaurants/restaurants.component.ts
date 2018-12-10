2bb78a964f9f668705d95ed182683112a69d8c3b
italokrenak <italokrenak@gmail.com>
2018-12-09 21:17:26 -0300

    Preventing Interruption Observable Values (catch oparator)

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -5,6 +5,10 @@ import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
+
+import { Observable } from 'rxjs/Observable';
 
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurants.service';
@@ -50,7 +54,9 @@ export class RestaurantsComponent implements OnInit {
             .debounceTime(500)
             .distinctUntilChanged()
             .switchMap(searchTerm =>
-                this.restaurantService.restaurants(searchTerm))
+                this.restaurantService
+                    .restaurants(searchTerm)
+                    .catch(error => Observable.from([])))
             .subscribe(restaurants => this.restaurants = restaurants);
 
         this.restaurantService.restaurants()
