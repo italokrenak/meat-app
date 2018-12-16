833286f7b8c782653927f156a540ed7c8f9eaeaf
italokrenak <italokrenak@gmail.com>
2018-12-16 19:10:22 -0300

    Implementing Error Handler Global

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,20 +1,33 @@
 import { HttpErrorResponse } from "@angular/common/http";
-import { Observable } from "rxjs/Observable";
 import 'rxjs/add/observable/throw';
+import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { NotificationService } from "./shared/notification.service";
+import { LoginService } from "./security/login/login.service";
 
-export class ErrorHandler {
-    static handleError(error: HttpErrorResponse | any) {
-        let errorMessage: string
+@Injectable()
+export class ApplicationErrorHandler extends ErrorHandler {
 
-        if (error instanceof HttpErrorResponse) {
-            const body = error.error;
-            errorMessage =
-                `${error.url}: ${error.status} - ${error.status || ''} ${body}`;
-        } else {
-            errorMessage = error.errorMessage ? error.errorMessage : error.toString();
-        }
-
-        console.log(errorMessage)
-        return Observable.throw(errorMessage);
+    constructor(private ns: NotificationService,
+        private injector: Injector) {
+        super();
     }
+
+    handleError(errorResponse: HttpErrorResponse | any) {
+        if (errorResponse instanceof HttpErrorResponse) {
+            const message = errorResponse.error.message;
+            switch (errorResponse.status) {
+                case 401:
+                    this.injector.get(LoginService).handleLogin();
+                    break;
+                case 403:
+                    this.ns.notify(message || 'Não autorizado.');
+                    break;
+                case 404:
+                    this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.');
+                    break;
+            }
+        }
+        super.handleError(errorResponse);
+    }
+
 }
