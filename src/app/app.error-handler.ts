aa3b63c610cd16883b5c3f1466be6e0d686e0223
italokrenak <italokrenak@gmail.com>
2018-12-17 18:40:10 -0300

    Using Zones

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import 'rxjs/add/observable/throw';
-import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { ErrorHandler, Injectable, Injector, NgZone } from "@angular/core";
+
 import { NotificationService } from "./shared/notification.service";
 import { LoginService } from "./security/login/login.service";
 
@@ -8,24 +9,27 @@ import { LoginService } from "./security/login/login.service";
 export class ApplicationErrorHandler extends ErrorHandler {
 
     constructor(private ns: NotificationService,
-        private injector: Injector) {
+        private injector: Injector,
+        private zone: NgZone) {
         super();
     }
 
     handleError(errorResponse: HttpErrorResponse | any) {
         if (errorResponse instanceof HttpErrorResponse) {
             const message = errorResponse.error.message;
-            switch (errorResponse.status) {
-                case 401:
-                    this.injector.get(LoginService).handleLogin();
-                    break;
-                case 403:
-                    this.ns.notify(message || 'Não autorizado.');
-                    break;
-                case 404:
-                    this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.');
-                    break;
-            }
+            this.zone.run(() => {
+                switch (errorResponse.status) {
+                    case 401:
+                        this.injector.get(LoginService).handleLogin();
+                        break;
+                    case 403:
+                        this.ns.notify(message || 'Não autorizado.');
+                        break;
+                    case 404:
+                        this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.');
+                        break;
+                }
+            });
         }
         super.handleError(errorResponse);
     }
